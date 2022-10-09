0715456fc6b2d50982b2af34563697101978556a
Johnny <JohnnyLieu@yahoo.com>
2022-10-08 20:54:37 -0700

    added a field and submit button for new posts

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ app.get('/contact', function(req, res){
   res.render('contact', {contactContent: contactContent});
 })
 
+app.get('/compose', function(req, res){
+  res.render('compose');
+})
+
 app.listen(port, function() {
   console.log(`Server started on port ${port}`);
 });
